56b0e6dd5d0171eed0fad07ddfe617234abe580d
souhel00 <souhail.semraoui@etu.univ-lyon1.fr>
2026-03-13 10:48:05 +0100

    new

--- src/main/java/com/colocmanager/Main.java
+++ src/main/java/com/colocmanager/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("ColocManager started");
+        System.out.println("ColocManager start");
 
     }
 
